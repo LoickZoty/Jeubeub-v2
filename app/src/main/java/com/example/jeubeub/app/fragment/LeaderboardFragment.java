33d1b6b757a410c4f6cdafe7b55dbb7c9399b9ca
loick <loick13009@live.fr>
2021-11-19 19:52:14 +0100

    Morpion presque OK, quelques bug à debug + ajout d'un popup de fin de partie

--- app/src/main/java/com/example/jeubeub/app/fragment/LeaderboardFragment.java
+++ app/src/main/java/com/example/jeubeub/app/fragment/LeaderboardFragment.java
@@ -1,5 +1,10 @@
 package com.example.jeubeub.app.fragment;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.jeubeub.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.games.AnnotatedData;
+import com.google.android.gms.games.FriendsResolutionRequiredException;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.PlayerBuffer;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Objects;
 
 
 public class LeaderboardFragment extends Fragment {
@@ -17,6 +30,20 @@ public class LeaderboardFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        View view = inflater.inflate(R.layout.fragment_leaderboard, container, false);
+        showLeaderboard(requireActivity(), requireContext());
+        return view;
+    }
+
+    private void showLeaderboard(Activity activity, Context context) {
+        Games.getLeaderboardsClient(activity, Objects.requireNonNull(GoogleSignIn.getLastSignedInAccount(context)))
+                .getLeaderboardIntent("CgkI3rSPs44MEAIQAg")
+                .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                    @Override
+                    public void onSuccess(Intent intent) {
+                        startActivityForResult(intent, 9004);
+                    }
+                }).addOnFailureListener(e -> System.out.println(e.getMessage()));
     }
 }
+
